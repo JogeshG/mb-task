94e476419c88111c2b0a00890aebcbf847d1fc1a
JogeshG <71059680+JogeshG@users.noreply.github.com>
2020-09-13 15:51:10 +0530

    mb-task first build

---
+++ build/precache-manifest.a357a9169219bcbc238f8acc92ba15d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebfb518a569c5f0eb1ff25088e83d5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2137d289c0821aeba2b6",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "865995ba6fbc182f14bd",
+    "url": "/static/css/main.573baa50.chunk.css"
+  },
+  {
+    "revision": "2137d289c0821aeba2b6",
+    "url": "/static/js/2.b83b69a8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.b83b69a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865995ba6fbc182f14bd",
+    "url": "/static/js/main.f6968049.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
